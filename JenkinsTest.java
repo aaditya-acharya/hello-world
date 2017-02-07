52a6033ab36bafe65277d57750518664a84b8bd0
aaditya-acharya <aaditya.achrya@gmail.com>
2017-02-07 14:32:41 +0530

    Add files via upload

---
+++ JenkinsTest.java
@@ -0,0 +1,49 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class JenkinsTest {
+	private static final String FILENAME = "C:\\test\\"+ new SimpleDateFormat("yyyyMMddhhmm'.txt'").format(new Date());
+
+	public static void main(String[] args) {
+
+		BufferedWriter bw = null;
+		FileWriter fw = null;
+
+		try {
+
+			String content = "This is the content to write into file\n";
+
+			fw = new FileWriter(FILENAME);
+			bw = new BufferedWriter(fw);
+			bw.write(content);
+
+			System.out.println("Done");
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+
+				if (bw != null)
+					bw.close();
+
+				if (fw != null)
+					fw.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+
+		}
+
+	}
+
+}
